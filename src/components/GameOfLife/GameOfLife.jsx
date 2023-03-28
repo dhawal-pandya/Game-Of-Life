d1b513a3abd442265c598ea56e98cf48a2533d56
Dhawal Pandya <95483771+dhawal-pandya@users.noreply.github.com>
2023-03-29 02:33:46 +0530

    Updated some text in Game of Life component

--- src/components/GameOfLife/GameOfLife.jsx
+++ src/components/GameOfLife/GameOfLife.jsx
@@ -9,19 +9,28 @@ const GameOfLife = () => {
       <Grid />
       <footer>
         <div>
-          {'Made by '}
-          <a target='_blank' href='https://dhawal-pandya.github.io/Portfolio/'>
+          {'Made with ❤️ by '}
+          <a
+            target='_blank'
+            href='https://dhawal-pandya.github.io/'
+            rel='noreferrer'
+          >
             Dhawal Pandya
           </a>
           &nbsp;|&nbsp;
           {/* &nbsp; just gives a space, that's it. */}
-          <a target='_blank' href='https://github.com/dhawal-pandya'>
+          <a
+            target='_blank'
+            href='https://github.com/dhawal-pandya/Game-Of-Life'
+            rel='noreferrer'
+          >
             Source
           </a>
           &nbsp;|&nbsp;
           <a
             target='_blank'
             href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life'
+            rel='noreferrer'
           >
             To learn more...
           </a>
